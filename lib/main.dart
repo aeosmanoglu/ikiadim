bc9dfc5a6ec15c47e3f27bb537d253a84009a6a7
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2021-10-27 03:53:59 +0300

    Hive secured

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:ikiadim/model/onetimepass.dart';
 import 'package:ikiadim/view/list.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // FlutterSecureStorage
+  FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+  bool containsEncryptionKey = await secureStorage.containsKey(key: 'key');
+  if (!containsEncryptionKey) {
+    List<int> key = Hive.generateSecureKey();
+    await secureStorage.write(key: 'key', value: base64UrlEncode(key));
+  }
+  String? value = await secureStorage.read(key: 'key');
+  Uint8List encryptionKey = base64Url.decode(value!);
+
+  // Hive
   await Hive.initFlutter();
   Hive.registerAdapter(OneTimePasswordAdapter());
   Hive.registerAdapter(PasswordAdapter());
-  await Hive.openBox<OneTimePassword>(HiveBoxes.box);
+  await Hive.openBox<OneTimePassword>(
+    HiveBoxes.box,
+    encryptionCipher: HiveAesCipher(encryptionKey),
+  );
+
   runApp(const MyApp());
 }
 
@@ -30,7 +51,6 @@ class MyApp extends StatelessWidget {
 /// - add try - catch
 /// - add sentry
 /// - delete list row
-/// - secure hive
 /// - secure app
 /// - read qr code
 /// - name
